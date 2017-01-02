cdd636a68dd49e4aa77749055ca3fcd23a967b0f
GCJ <kongsophanit221@gmail.com>
2017-01-01 19:31:32 -0800

    First Commit

---
+++ GoogleMaterialDesign/app/src/main/java/android/support/v7/widget/Toolbar.java
@@ -0,0 +1,14 @@
+package android.support.v7.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by GCJ on 0001-Jan-01-2017.
+ */
+public class Toolbar extends View {
+    public Toolbar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
